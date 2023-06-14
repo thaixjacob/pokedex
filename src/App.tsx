4ca83502855a9063cc2efdc9e9db1fb36c8fd8f8
`thais` <`tjlannes@gmail.com`>
2023-06-14 11:00:01 +0200

    update type of image of the logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <img src="/assets/pokedex-logo.png" alt="Pokédex logo"></img>
+      <img src="/assets/pokedex-logo.svg" alt="Pokédex logo"></img>
       <h1>Coming soon!</h1>
     </div>
   );
