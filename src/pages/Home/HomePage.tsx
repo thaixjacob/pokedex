c193b05a2762f44f6787de894774fda5531f9bfe
`thais` <`tjlannes@gmail.com`>
2023-06-21 19:24:34 +0200

    create home page

---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,14 @@
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="App">
+      <section className="temporally-homepage">
+        <img src="././assets/images/pokedex-logo.svg" alt="Pokédex logo"></img>
+        <h1>Coming soon!</h1>
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
