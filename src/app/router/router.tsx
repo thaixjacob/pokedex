e6395357af515d13ed2acfea44b1057f0df7de38
`thais` <`tjlannes@gmail.com`>
2023-06-26 15:57:25 +0200

    update routers with NotFound

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from '../../pages/Home/HomePage';
 import MainLayout from '../layout/MainLayout';
+import NotFound from '../../pages/NotFound/NotFound';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '',
